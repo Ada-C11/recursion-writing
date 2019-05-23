3424e9b3c027673d6eb12a8306d461857c460163
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-23 12:17:22 -0700

    question 1 solution added

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,15 +1,21 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+  if n == 0
+    return 1
+  elsif n < 0
+    raise ArgumentError
+  else
+    return n * factorial(n - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+  
 end
 
 # Time complexity: ?
