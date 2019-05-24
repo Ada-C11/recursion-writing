737ae875caba500b20cca27c133f7261a6c1092f
Ngoc Le <ngocle@outlook.com>
2019-05-23 22:27:51 -0700

    added factoria, reverse, bunny methods and updated testing to test reverse method in place

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,49 +1,89 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+  if n < 0
+    raise ArgumentError
+  end
+  if n == 0
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+  if s.empty?
+    return ""
+  end
+  if s.length == 1
+    return s[0]
+  else
+    return s[s.length - 1] + reverse(s[0..s.length - 2])
+  end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1)
+def reverse!(s, b, e)
+  if b >= e
+    return
+  else
+    temp = s[b]
+    s[b] = s[e]
+    s[e] = temp
+    reverse!(s, b + 1, e - 1)
+  end
+end
+
 def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+  if s.empty?
+    return ""
+  end
+  if s.length == 1
+    return s[0]
+  end
+  b = 0
+  e = s.length - 1
+  reverse!(s, b, e)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+  if n == 0
+    return 0
+  end
+  if n == 1
+    return 2
+  else
+    return 2 + bunny(n - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+  raise NotImplementedError, "Method not implemented"
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+  raise NotImplementedError, "Method not implemented"
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+  raise NotImplementedError, "Method not implemented"
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+  raise NotImplementedError, "Method not implemented"
 end
