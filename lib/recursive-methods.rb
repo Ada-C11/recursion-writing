422b555fb0b5ca05d5c60f8ce044c1a36090062f
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-23 21:31:57 -0700

    completed recurisive functions and passing all tets

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -3,47 +3,101 @@
 # Time complexity: ?
 # Space complexity: ?
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+  if n < 0
+    raise ArgumentError
+  elsif n == 1 || n == 0
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+  if s.length == 0 || s.length == 1
+    return s
+  else
+    return s[-1] + (reverse(s[0..-2]))
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+
+# helper method for keeping track of these indexes!
+def reverse_inplace(s, i = 0, j = s.length - 1)
+  if i >= j
+    return s
+  else
+    temp = s[i]
+    s[i] = s[j]
+    s[j] = temp
+
+    return reverse_inplace(s, i += 1, j -= 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+  if n == 0
+    return n
+  elsif n == 1
+    return 2
+  else
+    return 2 + bunny(n - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+  hash = { "(" => ")" }
+
+  if s == ""
+    return true
+  elsif s.length == 1 || hash[s[0]] != s[-1]
+    return false
+  else
+    return nested(s[1..-2])
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+  if array.empty?
+    return false
+  elsif array[0] == value
+    return true
+  else
+    return search(array[1..-1], value)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+  if s == "" || s.length == 1
+    return true
+  elsif s[0] != s[-1]
+    return false
+  else
+    return is_palindrome(s[1..-2])
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+  if (n / 10 == 0) || (m / 10 == 0)
+    return (n % 10 != m % 10) ? 0 : 1
+  else
+    if (n % 10 == m % 10)
+      return 1 + digit_match(n / 10, m / 10)
+    else
+      return digit_match(n / 10, m / 10)
+    end
+  end
 end
