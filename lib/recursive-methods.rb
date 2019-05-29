4e4c7d365f92bdbeca86555619106d90b0c0d829
Erica Norris <norrise120@gmail.com>
2019-05-29 15:40:09 -0700

    added in time and space complexity comments

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,7 +1,11 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# NOTE: I tried my best with time and space complexity, but it just feels so strange with recursive methods.  Do you have any recommended resources
+# discussion time/space complexity, specifically with recursive methods?
+
+# Time complexity: O(n), where n is the number being passed in
+# Space complexity: O(n), where n is the number being passed in.  The method returns n * method(n-1).
+# So for 4, it would have to store:  4 * method(3), 3 * method(2), 2 * method(1), 1 * method(0) * 0.
 def factorial(n)
   raise ArgumentError if n < 0
 
@@ -12,8 +16,9 @@ def factorial(n)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), where n is the length of the string
+# Space complexity: I'm not sure what the space complexity is.  I know that it is at least linear for the stack.  I am guessing that it might be O(n^2)
+# take into account that we are creating a new string each time
 def reverse(s)
   if s == ""
     return ""
@@ -22,8 +27,8 @@ def reverse(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the length of the string.
+# Space complexity: O(n), where n is the length of the string. No extra space on top of the stack as the string is being changed in place
 def reverse_inplace(s, high_index: nil, low_index: nil)
   high_index ||= s.length - 1
   low_index ||= 0
@@ -35,8 +40,8 @@ def reverse_inplace(s, high_index: nil, low_index: nil)
   return reverse_inplace(s, high_index: high_index, low_index: low_index)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the size of the number
+# Space complexity: O(n), where n is the size of the number
 def bunny(n)
   if n == 0
     return 0
@@ -45,8 +50,8 @@ def bunny(n)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), where n is the size of the string.
+# Space complexity: O(n^2), where n is the size of the string. Similarly to reverse(s) I think
 def nested(s)
   if s == ""
     return true
@@ -57,8 +62,8 @@ def nested(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), where n is the size of the string.
+# Space complexity: O(n^2), where n is the size of the string. Similarly to reverse(s) I think
 def search(array, value)
   if array.length == 0
     return false
@@ -69,8 +74,8 @@ def search(array, value)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n^2), where n is the size of the string.
+# Space complexity: O(n^2), where n is the size of the string.
 def is_palindrome(s)
   if s.length == 1 || s.length == 0
     return true
@@ -81,8 +86,8 @@ def is_palindrome(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(log[10] n), where n is proportional to the size of the number and smaller than m
+# Space complexity: O(log[10] n), where n is proportional to the size of the number and smaller than m
 def digit_match(n, m)
   if n / 10 == 0 || m / 10 == 0
     if n % 10 == m % 10
