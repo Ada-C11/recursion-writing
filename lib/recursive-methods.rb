4d1bb2d91e5ad663320a6f362d689fb9231f1c84
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 21:38:05 -0700

    added time/space complexity explanations

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,8 +1,8 @@
 require "pry"
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n recursive calls are made
+# Space complexity: O(n) where the stack is n levels deep
 def factorial(n)
   raise ArgumentError if n < 0
   if (n == 1) || (n == 0)
@@ -12,8 +12,8 @@ def factorial(n)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n recursive calls are made
+# Space complexity: O(n) where n levels are required, each requiring 1 char less of memory
 def reverse(s)
   if s.length == 1 || s.length == 0
     return s
@@ -22,8 +22,8 @@ def reverse(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n/2 recursive calls are made
+# Space complexity: O(n) where the max depth of the function is n/2
 def reverse_inplace(s)
   if s.length == 1 || s.length == 0
     return s
@@ -32,20 +32,18 @@ def reverse_inplace(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n recursive calls are made
+# Space complexity: O(n) where the max depth is n
 def bunny(n)
   if n <= 0
     return 0
-  elsif n == 1
-    return 2
   else
     return 2 + bunny(n - 1)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n/2 recursive calls are made
+# Space complexity: O(n) where the max depth of the function is n/2
 def nested(s)
   if s.length == 0
     return true
@@ -58,8 +56,8 @@ def nested(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because there are 2n-1 calls each time.
+# Space complexity: O(log n) because at most the algorithm goes log2 n levels deep
 def search(array, value)
   return false if array.length == 0
   if array.length == 1
@@ -72,8 +70,8 @@ def search(array, value)
   return false
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n/2 recursive calls are made
+# Space complexity: O(n) where the max depth of the function is n/2
 def is_palindrome(s)
   if s.length == 0 || s.length == 1
     return true
@@ -84,8 +82,8 @@ def is_palindrome(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: If n < m, time complexity will be O(log10 n) since the number of calls is dependent on how many times the smaller number can be divided by 10
+# Space complexity: also O(log10 n) since the depth is proportional to the number of calls
 def digit_match(n, m)
   match_value = (n % 10 == m % 10) ? 1 : 0
   if n / 10 == 0 || m / 10 == 0
